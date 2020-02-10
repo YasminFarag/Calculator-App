fac6bbe43ba147b7d78373d11acfa7996c9128b8
Yasmin <yasmin@Yasmins-MacBook-Air.local>
2020-02-10 20:39:45 +0100

    calculator works

--- src/App.js
+++ src/App.js
@@ -1,22 +1,53 @@
-import React, {Component} from 'react';
-import './App.css';
-import Result from './components/Result';
+import React, { Component } from "react";
+import "./App.css";
+import Result from "./components/Result";
 
 class App extends Component {
-  state = { result : '' }
+  state = { result: "", previousResult: "", operation: "" };
+  clear = () => {
+    this.setState({
+      result: ""
+    });
+  };
 
-  
+  calculate = () => {
+    const newResult = this.state.result;
+    this.setState({
+      result: eval(newResult).toString()
+    });
+  };
 
- onClick = () => {
+  dot = () => {
+    const array = this.state.result.split(" ");
+    const lastString = array[array.length - 1];
+    if (lastString.indexOf(".") === -1) {
+      this.setState({
+        result: this.state.result + "."
+      });
+    }
+  };
 
- }
-  render() { 
-    return ( <div className='App'>
-      <h1>Calculator</h1>
-      <Result result = {this.state.result}
-              onClick = {this.onClick} />
-    </div> );
+  onClick = button => {
+    if (button === "C") {
+      this.clear();
+    } else if (button === "=") {
+      this.calculate();
+    } else if (button === ".") {
+      this.dot();
+    } else {
+      this.setState({
+        result: this.state.result + button
+      });
+    }
+  };
+  render() {
+    return (
+      <div className="App">
+        <h1>Calculator</h1>
+        <Result result={this.state.result} onClick={this.onClick} />
+      </div>
+    );
   }
 }
- 
+
 export default App;
